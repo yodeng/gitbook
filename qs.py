0bb6fa779787d0655da1dbce983ee5855929f5c4
yodeng <393548812@qq.com>
2018-12-04 21:52:34 +0800

    add qstat command not found situation

--- qs.py
+++ qs.py
@@ -6,6 +6,7 @@ For summary all jobs through qstat command
 
 import os
 import sys
+import psutil
 from collections import defaultdict
 from commands import getstatusoutput
 
@@ -16,9 +17,9 @@ def style(string, mode = '', fore = '', back = ''):
     'mode':{'mormal':0,'bold':1,'underline':4,'blink':5,'invert':7,'hide':8},
     'default':{'end':0},
     }
-    mode  = '%s' % STYLE['mode'][mode] if STYLE['mode'].has_key(mode) else ''
-    fore  = '%s' % STYLE['fore'][fore] if STYLE['fore'].has_key(fore) else ''
-    back  = '%s' % STYLE['back'][back] if STYLE['back'].has_key(back) else ''
+    mode  = '%s' % STYLE["mode"].get(mode,"")
+    fore  = '%s' % STYLE['fore'].get(fore,"")
+    back  = '%s' % STYLE['back'].get(back,"")
     style = ';'.join([s for s in [mode, fore, back] if s])
     style = '\033[%sm' % style if style else ''
     end   = '\033[%sm' % STYLE['default']['end'] if style else ''
@@ -33,9 +34,9 @@ def main():
         for j in alljobs:
             j = j.split()
             jobs.setdefault(j[3],defaultdict(int))[j[4]] += 1
-        print style("-"*48,mode="bold")
+        print style("-"*47,mode="bold")
         print style("{0:<20} {1:>8} {2:>8} {3:>8}".format("user","jobs","run","queue"),mode="bold")
-        print style("-"*48,mode="bold")
+        print style("-"*47,mode="bold")
         for u in sorted(jobs.items(),key=lambda x:sum(x[1].values()),reverse=True):
             user = u[0]
             job = sum(u[1].values())
@@ -45,10 +46,30 @@ def main():
                 print style("{0:<20} {1:>8} {2:>8} {3:>8}".format(user,job,run,qw),fore="red",mode="bold")
             else:
                 print style("{0:<20} {1:>8} {2:>8} {3:>8}".format(user,job,run,qw))
-        print style("-"*48,mode="bold")
+        print style("-"*47,mode="bold")
     else:
-        print style("Error: qstat command not found",fore="red",mode="bold")
-        sys.exit(1)
+        users = map(lambda x:os.path.basename(x),os.popen('ls -d /home/*').read().split())
+        allpids = psutil.pids()
+        jobs = {}
+        for p in allpids:
+            try:
+                j = psutil.Process(p)
+                if j.username() not in users:
+                    continue
+                jobs.setdefault(j.username(),defaultdict(int))[j.status()] += 1
+            except:
+                continue                
+        print style("-"*50,mode="bold")
+        print style("{0:<20} {1:>5} {2:>5} {3:>5} {4:>5} {5:>5}".format("user","jobs","R","S","D","Z"),mode="bold")
+        print style("-"*50,mode="bold")
+        for u in sorted(jobs.items(),key=lambda x:sum(x[1].values()),reverse=True):
+            user = u[0]
+            job = sum(u[1].values())
+            if user == username:
+                print style("{0:<20} {1:>5} {2:>5} {3:>5} {4:>5} {5:>5}".format(user,job,u[1].get("running",0),u[1].get("sleeping",0),u[1].get("dead",0),u[1].get("zombie",0)),fore="red",mode="bold")
+            else:
+                print style("{0:<20} {1:>5} {2:>5} {3:>5} {4:>5} {5:>5}".format(user,job,u[1].get("running",0),u[1].get("sleeping",0),u[1].get("dead",0),u[1].get("zombie",0)))
+        print style("-"*50,mode="bold")
         
 if __name__ == "__main__":
     main()
